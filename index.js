7b6cee12991e89145f8441df2ef59539c8031fbb
Hamidul-1 <hamidul786cse@gmail.com>
2022-05-19 03:52:15 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello to do app!')
+})
+
+app.listen(port, () => {
+    console.log(`To do app listening on port ${port}`)
+})
