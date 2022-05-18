aafd7c3f528b9aa60b91e47a935504ab3066b270
Hamidul-1 <hamidul786cse@gmail.com>
2022-05-19 05:44:51 +0600

    adding steps

--- index.js
+++ index.js
@@ -58,6 +58,10 @@ app.get('/', (req, res) => {
     res.send('Hello to do app!')
 })
 
+app.get('/hero', (req, res) =>{
+    res.send('Hero meets hero ku')
+})
+
 app.listen(port, () => {
     console.log(`To do app listening on port ${port}`)
 })
